e391b26cbe9a4f7fa5fce1afb18dafdcf973d963
Youssef Amr <yosefamr2002@icloud.com>
2025-05-22 01:35:41 +0300

    post popup added

---
+++ js/home.js
@@ -0,0 +1,18 @@
+  const newpost_input = document.querySelector(".newpost_input");
+  const postPopup = document.getElementById("postPopup");
+  const closePopup = document.getElementById("closePopup");
+
+  newpost_input.addEventListener("click", () => {
+    postPopup.style.display = "flex";
+  });
+
+  closePopup.addEventListener("click", () => {
+    postPopup.style.display = "none";
+  });
+
+  //close when user click in anywhere in screen 
+  window.addEventListener("click", (e) => {
+    if (e.target === postPopup) {
+      postPopup.style.display = "none";
+    }
+  });
